2a6e6fbeef2f79f72fef7ffe06eca802e6062d10
VdymHunter <vadimnaumnaum@gmail.com>
2023-10-21 03:27:49 +0200

    Custom BeanFectoryPostProcessor in xml file. 13

---
+++ src/main/java/com/myspringproject/bfpp/LogBeanFactoryPostProcessors.java
@@ -0,0 +1,23 @@
+package com.myspringproject.bfpp;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.PriorityOrdered;
+import org.springframework.core.annotation.Order;
+
+import java.lang.annotation.Annotation;
+
+public class LogBeanFactoryPostProcessors implements BeanFactoryPostProcessor, PriorityOrdered {
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println();
+    }
+
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
