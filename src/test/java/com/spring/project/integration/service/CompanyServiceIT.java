e84e6af83871ba61fce95d12ad7501ee03ca832a
VdymHunter <vadimnaumnaum@gmail.com>
2023-10-28 02:57:48 +0200

    Integration testing part 3 . 44

--- src/test/java/com/spring/project/integration/service/CompanyServiceIT.java
+++ src/test/java/com/spring/project/integration/service/CompanyServiceIT.java
@@ -1,17 +1,22 @@
 package com.spring.project.integration.service;
 
 import com.spring.project.ApplicationRunner;
+import com.spring.project.config.DatabaseProperties;
 import com.spring.project.database.entity.Company;
 import com.spring.project.dto.CompanyReadDto;
+import com.spring.project.integration.annotation.IT;
 import com.spring.project.listener.entity.EntityEvent;
 import com.spring.project.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,12 +26,14 @@ import static org.mockito.Mockito.*;
 
 //@ExtendWith(SpringExtension.class)
 //@ContextConfiguration(classes = ApplicationRunner.class,initializers = ConfigDataApplicationContextInitializer.class)
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
+//@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 public class CompanyServiceIT {
     private static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    private CompanyService companyService;
+    private final DatabaseProperties databaseProperties;
+    private final CompanyService companyService;
     @Test
     void findById(){
 
