dc419e1948a9b284ac60eed6b7d8b9a2a008d3e6
Vdym <vadimnaumnaum@gmail.com>
2023-12-15 01:30:50 +0100

    OAuth-2.0.-Authentication-Principle.113

--- src/main/java/com/spring/project/config/SecurityConfiguration.java
+++ src/main/java/com/spring/project/config/SecurityConfiguration.java
@@ -1,65 +1,72 @@
 package com.spring.project.config;
 
+import com.spring.project.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Set;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
-//    @Bean
-//    public AuthorizationFilter authorizationFilter(A)
+    private final UserService userService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests(authUrl-> authUrl
-                .requestMatchers(antMatcher("/login"),antMatcher("/users/registration"),antMatcher("/v3/api-docs/**"),antMatcher("/swagger-ui/**")).permitAll()
-                                .requestMatchers(antMatcher("/users/{\\d+}/delete")).hasAuthority("ADMIN")
-                                .requestMatchers(antMatcher("/admin/**")).hasAuthority("ADMIN")
-                                .anyRequest().authenticated()
+                .authorizeHttpRequests(authUrl -> authUrl
+                        .requestMatchers(antMatcher("/login"), antMatcher("/users/registration"), antMatcher("/v3/api-docs/**"), antMatcher("/swagger-ui/**")).permitAll()
+                        .requestMatchers(antMatcher("/users/{\\d+}/delete")).hasAuthority("ADMIN")
+                        .requestMatchers(antMatcher("/admin/**")).hasAuthority("ADMIN")
+                        .anyRequest().authenticated()
                 )
-//                .anyRequest().authenticated()
-//                .and()
-//                .httpBasic(Customizer.withDefaults());
-                .logout(logout->logout
+                .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/login")
                         .deleteCookies("JSESSIONID"))
                 .formLogin(login -> login
                         .loginPage("/login")
-                        .defaultSuccessUrl("/users").permitAll());
+                        .defaultSuccessUrl("/users"))
+                .oauth2Login(config -> config
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/users")
+                        .userInfoEndpoint(userInfo -> userInfo.oidcUserService(oidcUserService())));
+//.formLogin(login -> login
+//                .loginPage("/login")
+//                .defaultSuccessUrl("/users").permitAll())
 
-//                .requestMatchers("/api/v1/auth/**")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authenticationProvider(authenticationProvider)
-//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-//             .authorizeHttpRequests()
-//                        .requestMatchers("/login","/users/registration","/v3/api-docs/**","/swagger-ui/**").permitAll()
-//                        .requestMatchers("/users/{\\d+}/delete").hasAuthority("ADMIN")
-//                        .requestMatchers("/admin/**").hasAuthority("ADMIN")
-//                        .anyRequest().authenticated()
-//                .and()
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder();
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return userRequest -> {
+            String email = userRequest.getIdToken().getClaims().get("email").toString();
+            //todo create user userService.create
+            UserDetails userDetails = userService.loadUserByUsername(email);
+//            new OidcUserService().loadUser()
+            DefaultOidcUser oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken());
+            Set<Method> userDetailsMethods = Set.of(UserDetails.class.getMethods());
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(), new Class[]{UserDetails.class, OidcUser.class},
+                    (proxy, method, args) -> userDetailsMethods.contains(method)
+                            ? method.invoke(userDetails, args)
+                            : method.invoke(oidcUser, args));
+        };
     }
+
 
 }
